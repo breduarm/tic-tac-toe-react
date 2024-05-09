c32483088ce8938e79490a8ff3a26546deb8e745
breduarm <bryan.e.armijos@gmail.com>
2024-05-09 18:57:31 -0500

    Create Board and Square and implement basic click functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,44 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(index) {
+    if (squares[index]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[index] = "X";
+    } else {
+      nextSquares[index] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext)
+  };
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onClickSquare={() => handleClick(0)} />
+        <Square value={squares[1]} onClickSquare={() => handleClick(1)} />
+        <Square value={squares[2]} onClickSquare={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onClickSquare={() => handleClick(3)} />
+        <Square value={squares[4]} onClickSquare={() => handleClick(4)} />
+        <Square value={squares[5]} onClickSquare={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onClickSquare={() => handleClick(6)} />
+        <Square value={squares[7]} onClickSquare={() => handleClick(7)} />
+        <Square value={squares[8]} onClickSquare={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+function Square({ value, onClickSquare }) {
+  return <button className="square" onClick={onClickSquare}>{value}</button>
 }
