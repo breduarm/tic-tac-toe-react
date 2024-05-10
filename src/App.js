efb74984c7d057f0361175c8874116824c22f66d
breduarm <bryan.e.armijos@gmail.com>
2024-05-09 23:55:52 -0500

    Add a todo list for next step to improve the game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import { useState } from "react";
 
+/*
+TODOs
+- Solo para el movimiento actual, muestra «Estás en el movimiento #…» en lugar de un botón
+- Vuelve a escribir Board para usar dos bucles para crear los cuadrados en lugar de codificarlos.
+- Agrega un botón de alternancia que te permita ordenar los movimientos en orden ascendente o descendente.
+- Cuando alguien gane, resalta los tres cuadrados que causaron la victoria (y cuando nadie gane, muestra un mensaje indicando que el resultado fue un empate).
+- Muestra la ubicación de cada movimiento en el formato (columna, fila) en la lista del historial de movimientos.
+*/
+
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
