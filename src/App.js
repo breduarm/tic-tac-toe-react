0cc6d156e04e2d2562e1da26acdd130d198d5546
breduarm <bryan.e.armijos@gmail.com>
2024-05-09 22:55:54 -0500

    Add calculateWinner

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import { useState } from "react";
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = "Ganador: " + winner;
+  } else {
+    status = "Siguiente jugador: " + (xIsNext ? "X" : "O");
+  }
 
   function handleClick(index) {
-    if (squares[index]) {
+    if (squares[index] || winner) {
       return;
     }
     const nextSquares = squares.slice();
@@ -18,8 +26,29 @@ export default function Board() {
     setXIsNext(!xIsNext)
   };
 
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onClickSquare={() => handleClick(0)} />
         <Square value={squares[1]} onClickSquare={() => handleClick(1)} />
